71d90f3c8e267ab418e4314ed0d6f55ff8e20e8c
natasak <natasak@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-09-24 08:50:15 +0000

    Minor changes, added as.symData.R; namespace includes S3 methods
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/clamix@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/as.symData.R
@@ -0,0 +1,31 @@
+as.symData <- function(L,nvnames= NULL){
+  # checks if L is list of symObjects
+  # makes symData if is true
+  # or returns FALSE
+  if (!is.list(L) && class(L)!="symObject")
+    return(FALSE)
+  else{
+    if (class(L)=="symObject")
+      L1 <- L
+    else
+      L1 <- L[[1]]
+    nCats <- NULL
+    nnames <- vector(mode="list",length(L1))
+    for (i in (1:length(L1))){
+      nCats <- c(nCats,length(L1[[i]])-1)
+      if (is.null(nvnames)){
+        nnames[[i]] <- names(L1[[i]])}
+    }
+    so <- empty.symObject(nCats)
+    namedSO <- so
+    names(namedSO) <- names(L1)
+    if (is.null(nvnames)) nvnames <- nnames
+    for (i in 1:nVar)
+      names(namedSO[[i]]) <- nvnames[[i]]
+    alpha <- rep(1,length(L1))/length(L1)
+    type <- "gDist"
+  }
+  newdata <- list(SOs=L,so=so,namedSO=namedSO,alpha=alpha,type=type)
+  class(newdata) <- "symData"
+  return(newdata)
+}
